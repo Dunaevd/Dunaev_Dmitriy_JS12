8d7e257a71bd605ddd9016ed9297d9a3313092be
Dunaevd <dunaevskiy@bk.ru>
2020-03-28 23:25:00 +0300

    commin lesson02

---
+++ lesson02/mine.js
@@ -0,0 +1,22 @@
+let money = 23000, income = 'фриланс', addExpenses = 'Интернет, Такси, Коммуналка', deposit = Boolean('myCash'),
+ mission = 200000, period = 12;
+ 
+ console.log(typeof money, typeof income, typeof deposit);
+
+ console.log(addExpenses.length);
+
+ console.log('Период равен' + ' ' + period + ' ' + 'месяцев');
+
+ console.log('Цель заработать' + ' ' + mission + ' ' + 'рублей');
+
+ console.log(addExpenses.toLowerCase());
+
+ let arr = addExpenses.split(',');
+
+ console.log(arr);
+
+ console.log(arr[0].toLowerCase(), arr[1].toLowerCase(), arr[2].toLowerCase());
+ 
+ let budgetDay = money / 30;
+
+ console.log('Дневной бюджет ' + budgetDay);
